ea641443db8663bb718599c07ec7dd5dd3d98abc
h0ngcha0 <lbs.lhc@gmail.com>
2024-03-21 22:41:01 +0100

    transfer to semi-random address

--- rhone-local-cluster/script/transfer.sh
+++ rhone-local-cluster/script/transfer.sh
@@ -6,10 +6,12 @@ NUMBER_OF_NODES=${1:-3}
 PORT_1=${2:-22973}
 INTERVAL_SECONDS=${3:-2}
 
+NETWORKS=("193maApeJWrz9GFwWCfa982ccLARVE9Y1WgKSJaUs7UAx" "1CQvSXsmM5BMFKguKDPpNUfw1idiut8UifLtT8748JdHc" "16fZKYPCZJv2TP3FArA9FLUQceTS9U8xVnSjxFG9MBKyY" "1FsroWmeJPBhcPiUr37pWXdojRBe6jdey9uukEXk1TheA")
+
 while [ true ]; do
     for i in $(seq 0 $((NUMBER_OF_NODES-1)));
     do
-        curl -X POST http://localhost:$((PORT_1+i))/wallets/local-cluster-test/transfer -d '{"destinations": [{"address": "193maApeJWrz9GFwWCfa982ccLARVE9Y1WgKSJaUs7UAx", "attoAlphAmount": "1000000000000000000"}]}'
+        curl -X POST http://localhost:$((PORT_1+i))/wallets/local-cluster-test/transfer -d '{"destinations": [{"address": "'${NETWORKS[$((RANDOM % 4))]}'", "attoAlphAmount": "1000000000000000000"}]}'
         echo
         sleep $INTERVAL_SECONDS
     done
--- rhone-local-cluster/script/unlock-wallets.sh
+++ rhone-local-cluster/script/unlock-wallets.sh
@@ -7,5 +7,5 @@ PORT_1=${2:-22973}
 
 for i in $(seq 0 $((NUMBER_OF_NODES-1)));
 do
-    curl -X POST http://localhost:22973/wallets/local-cluster-test/unlock -d '{"password": "password"}'
+    curl -X POST http://localhost:$((PORT_1+i))/wallets/local-cluster-test/unlock -d '{"password": "password"}'
 done
